2bd07e6f2cb79c4cafae1d6a6b3a188af2dfd9e8
Hoang Nguyen <171530173+hoangNguyenAngelhack@users.noreply.github.com>
2026-04-29 10:47:53 +0700

    remove  env

--- env.example.ts
+++ env.example.ts
@@ -1,17 +1,1 @@
 export const API_URL = 'https://api.creationbesties.com';
-// export const API_URL = 'http://192.168.110.226:3000';
-export const GOOGLE_WEB_CLIENT_ID =
-  '84542265333-201ls9jcohgg7f46n6061o2polo4r6ja.apps.googleusercontent.com';
-export const GOOGLE_IOS_CLIENT_ID =
-  '84542265333-3l1nne8jnjkmr3i9i21fo39uc5g9ih50.apps.googleusercontent.com';
-
-export const GOOGLE_ANDROID_CLIENT_ID =
-  '84542265333-hhbugfkmuuasu8a1r89u41594nockaka.apps.googleusercontent.com';
-export const DEEZER_API_URL_SEARCH = 'https://api.deezer.com/search';
-export const Ads_KEY = '';
-
-export const CLOUDINARY_API_KEY = '614464145237874';
-export const CLOUDINARY_CLOUD_NAME = 'dqivuzgre';
-export const CLOUDINARY_API_SECRET = 'Utb9TD-mCpNcJ44YJk378HmCS8U';
-export const EMAIL_ADMIN = 'oquetel@creationbesties.com';
-export const DEEZER_API_URL_CHART = 'https://api.deezer.com/chart';
